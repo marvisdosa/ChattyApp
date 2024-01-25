cb6297eee9b5404b44a7092cefa0ed7bf22a196c
Marvis Ighedosa <dosamarvis@gmail.com>
2024-01-25 11:56:16 +0100

    Initial Commit

---
+++ ChattyApp/ChattyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChattyAppApp.swift
+//  ChattyApp
+//
+//  Created by Marvis Ighedosa on 25/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChattyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
